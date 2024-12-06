56625ccde368e0d72c6489bad62471c065f68f7c
engrjeff <engineerjeff15@gmail.com>
2024-12-06 16:06:21 +0800

    add mobile nav

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { Header } from "@/components/shared/header"
+import { MobileNav } from "@/components/shared/mobile-nav"
 import { Sidebar } from "@/components/shared/sidebar"
 
 export default function AppLayout({
@@ -11,11 +12,12 @@ export default function AppLayout({
     <>
       <TooltipProvider>
         <Sidebar />
-        <main className="lg:pl-sidebar-width">
+        <main className="lg:pl-sidebar-width pb-12 lg:pb-0">
           <Header />
           <div className="container mx-auto grid h-full max-w-screen-md grid-rows-1 overflow-y-auto p-4 lg:p-6">
             {children}
           </div>
+          <MobileNav />
         </main>
       </TooltipProvider>
     </>
---
+++ components/shared/mobile-nav.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { FolderIcon, MusicIcon, SearchIcon, UsersIcon } from "lucide-react"
+
+const menuItems = [
+  {
+    path: "/",
+    label: "Browse",
+    Icon: SearchIcon,
+  },
+  {
+    path: "/songs",
+    label: "Songs",
+    Icon: MusicIcon,
+  },
+  {
+    path: "/collections",
+    label: "Collections",
+    Icon: FolderIcon,
+  },
+  {
+    path: "/members",
+    label: "Members",
+    Icon: UsersIcon,
+  },
+]
+
+export function MobileNav() {
+  const pathname = usePathname()
+
+  return (
+    <nav className="fixed inset-x-4 bottom-4 lg:hidden">
+      <ul className="bg-muted/80 mx-auto flex h-16 max-w-[425px] items-center justify-evenly gap-2 rounded-full p-2 backdrop-blur-lg">
+        {menuItems.map((menu) => (
+          <li key={`mobile-nav-${menu.label}`}>
+            <Link
+              href={menu.path}
+              className="group flex flex-col items-center gap-1 rounded-full"
+              data-active={
+                pathname === "/" && menu.path === "/"
+                  ? true
+                  : pathname.includes(menu.path) && menu.path !== "/"
+                    ? true
+                    : false
+              }
+            >
+              <div className="group-data-[active=true]:bg-primary/20 group-data-[active=true]:text-primary rounded-full px-3 py-1">
+                <menu.Icon size={18} />
+              </div>
+              <span className="text-muted-foreground group-data-[active=true]:text-foreground text-xs">
+                {menu.label}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
